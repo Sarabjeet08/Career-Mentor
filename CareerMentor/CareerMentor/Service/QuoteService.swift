3caf0a18ec157d2ea4c0013eb89d0125ef95cf04
Sarabjeet Singh <163313253+Sarabjeet08@users.noreply.github.com>
2025-04-23 01:14:54 -0400

    Update QuoteService.swift

--- CareerMentor/CareerMentor/Service/QuoteService.swift
+++ CareerMentor/CareerMentor/Service/QuoteService.swift
@@ -6,4 +6,28 @@
 //
 
 
-import Foundationclass QuoteService {    static let shared = QuoteService()    private init() {}    func fetchMotivationalQuote(completion: @escaping (Quote?) -> Void) {        let url = URL(string: "https://zenquotes.io/api/random")!        URLSession.shared.dataTask(with: url) { data, response, error in            if let data = data {                do {                    let quote = try JSONDecoder().decode([Quote].self, from: data)                    completion(quote.first)                } catch {                    print("Decoding error:", error)                    completion(nil)                }            } else {                print("Network error:", error ?? "Unknown error")                completion(nil)            }        }.resume()    }}
+import Foundation
+
+class QuoteService {
+    static let shared = QuoteService()
+    private init() {}
+
+    func fetchMotivationalQuote(completion: @escaping (Quote?) -> Void) {
+        let url = URL(string: "URL for API")!
+
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            if let data = data {
+                do {
+                    let quote = try JSONDecoder().decode([Quote].self, from: data)
+                    completion(quote.first)
+                } catch {
+                    print("Decoding error:", error)
+                    completion(nil)
+                }
+            } else {
+                print("Network error:", error ?? "Unknown error")
+                completion(nil)
+            }
+        }.resume()
+    }
+}
